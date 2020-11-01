3480b12f61b63f4a8bec075f98d7b93532c23f91
BougatayaAbd <abd.bougataya@gmail.com>
2020-11-01 11:49:25 +0100

    Version 02

--- app.py
+++ app.py
@@ -4,5 +4,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    names=["Abdoullah", "Abdourrahman", "Fatema", "Hamza", "Zainab"]
-    return render_template("index.html", names=names)
+    return render_template("index.html")
