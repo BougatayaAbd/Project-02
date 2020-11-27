8141fce88d2b7cf685c1b0c37d1441e1209ea060
BougatayaAbd <abd.bougataya@gmail.com>
2020-11-27 17:06:45 +0100

    --- Auto-Git Commit ---

--- desktopapp/index.js
+++ desktopapp/index.js
@@ -4,6 +4,14 @@ const url = require('url');
 function boot() {
     win = new BrowserWindow()
     win.loadURL(url.format({
+        pathname: 'products.html',
+        slashes: true
+    }))
+    win.loadURL(url.format({
+        pathname: 'seconnecter.html',
+        slashes: true
+    }))
+    win.loadURL(url.format({
         pathname: 'index.html',
         slashes: true
     }))
