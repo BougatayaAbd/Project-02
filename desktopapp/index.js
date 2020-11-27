292ece0e6eacb7415d8cc6c08cb964014a40ab4b
BougatayaAbd <abd.bougataya@gmail.com>
2020-11-27 17:36:50 +0100

    --- Auto-Git Commit ---

--- desktopapp/index.js
+++ desktopapp/index.js
@@ -1,20 +1,15 @@
 const {app, BrowserWindow} = require('electron');
 const url = require('url');
 
+let win = null
+
 function boot() {
-    win = new BrowserWindow()
-    win.loadURL(url.format({
-        pathname: 'products.html',
-        slashes: true
-    }))
-    win.loadURL(url.format({
-        pathname: 'seconnecter.html',
-        slashes: true
-    }))
-    win.loadURL(url.format({
-        pathname: 'index.html',
-        slashes: true
-    }))
+    win = new BrowserWindow({
+        width: 1366,
+        height: 768,
+        frame: false
+    })
+    win.loadURL('file://${__dirname}/index.html')
 }
 
 app.on('ready', boot)
