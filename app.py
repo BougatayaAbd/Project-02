9d719f9b30ff6a7f412fb7821f2250248256d2d8
BougatayaAbd <abd.bougataya@gmail.com>
2020-12-02 12:17:25 +0100

    --- Auto-Git Commit ---

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
+import yaml
 
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = ''
-app.config['MYSQL_USER'] = ''
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = ''
+db = yaml.load(open('db.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+
+mysql = MySQL(app)
 
 @app.route('/')
 def index():
@@ -53,6 +57,11 @@ def seconnecter():
         name = userDetails['name']
         email = userDetails['email']
         password = userDetails['password']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users(name, email, password) VALUES(%s, %s)", (name, email))
+        mysql.connection.commit()
+        cur.close()
+        return 'successe'
     return render_template('seconnecter.html')
 if __name__ == "__main__":
     app.run(debug=True)
