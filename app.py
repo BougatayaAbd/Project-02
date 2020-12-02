827d53f7f67b4f1a8c7f0eacc730143b1305976a
BougatayaAbd <abd.bougataya@gmail.com>
2020-12-02 19:20:18 +0100

    --- Auto-Git Commit ---

--- app.py
+++ app.py
@@ -10,41 +10,40 @@ app.config['MYSQL_PASSWORD'] = db['mysql_password']
 app.config['MYSQL_DB'] = db['mysql_db']
 
 mysql = MySQL(app)
+##########Best Books names#########
 
+beststory1 = "Histoire 01"
+beststory2 = "Histoire 02"
+beststory3 = "Histoire 03"
+
+##########Best Books prices#########
+
+beststory1price = 0
+beststory2price = 0
+beststory3price = 0
+
+####################################
+
+if (beststory1price == 0):
+    beststory1price = str(beststory1price)
+    beststory1price = "Gratuit"
+else:
+    beststory1price = str(beststory1price)
+    beststory1price = beststory1price + " MAD"
+if (beststory2price == 0):
+    beststory2price = str(beststory2price)
+    beststory2price = "Gratuit"
+else:
+    beststory2price = str(beststory2price)
+    beststory2price = beststory2price + " MAD"
+if (beststory3price == 0):
+    beststory3price = str(beststory3price)
+    beststory3price = "Gratuit"
+else:
+    beststory3price = str(beststory3price)
+    beststory3price = beststory3price + " MAD"
 @app.route('/')
 def index():
-    ##########Best Books names#########
-
-    beststory1 = "Histoire 01"
-    beststory2 = "Histoire 02"
-    beststory3 = "Histoire 03"
-
-    ##########Best Books prices#########
-
-    beststory1price = 0
-    beststory2price = 0
-    beststory3price = 0
-
-    ####################################
-
-    if (beststory1price == 0):
-        beststory1price = str(beststory1price)
-        beststory1price = "Gratuit"
-    else:
-        beststory1price = str(beststory1price)
-        beststory1price = beststory1price + " MAD"
-    if (beststory2price == 0):
-        beststory2price = str(beststory2price)
-        beststory2price = "Gratuit"
-    else:
-        beststory2price = str(beststory2price)
-        beststory2price = beststory2price + " MAD"
-    if (beststory3price == 0):
-        beststory3price = str(beststory3price)
-        beststory3price = "Gratuit"
-    else:
-        beststory3price = str(beststory3price)
-        beststory3price = beststory3price + " MAD"
     return render_template("index.html", beststory1=beststory1, beststory2=beststory2, beststory3=beststory3, beststory1price=beststory1price, beststory2price=beststory2price, beststory3price=beststory3price)
 @app.route('/products')
 def products():
@@ -61,7 +60,6 @@ def seconnecter():
         cur.execute("INSERT INTO users(name, email, password) VALUES(%s, %s, %s)", (name, email, password))
         mysql.connection.commit()
         cur.close()
-        return 'successe'
     return render_template('seconnecter.html')
 if __name__ == "__main__":
     app.run(debug=True)
